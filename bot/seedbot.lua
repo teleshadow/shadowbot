b946059d2c3e2c26000aa4ddca2698701e3dd6a9
teleshadow <githuub@gmail.com>
2016-02-11 18:14:02 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,30 +227,17 @@ function create_config( )
     "admin"
     "spam"
     },
-    sudo_users = {147191022},--Sudo users
+    sudo_users = {147191022,120816252},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
-]],
+https://github.com/teleshadow/shadowbot,
+ Admins:
+@Xxx_mohammad_xxX
+@keraboy
+Thanks for you for your support '}, 
     help_text_realm = [[
 Realm Commands:
 
